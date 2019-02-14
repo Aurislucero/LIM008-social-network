f6e66a98db65023b80cae6941f9e4897f39a52b9
aurislucero <luro.corilla@gmail.com>
2019-02-13 22:38:39 -0500

    cambios guardados

--- src/templates/myprofile.js
+++ src/templates/myprofile.js
@@ -2,6 +2,7 @@ export const cargarMiPerfil = () => {
     const miPerfil = document.createElement('div');
     const datosUsuario = `
        <div>
+       
          <h4>Aqui van los datos del usuario</h4>
          <p>Hola Hola</p>
          <h4>Aqui van los datos del usuario</h4>
